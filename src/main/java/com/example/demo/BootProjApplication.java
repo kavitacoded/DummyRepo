265ea59147e1d57db038a4a4304170684c8e90ff
kavitacoded <kavi20900@gmail.com>
2024-04-29 15:12:26 +0530

    spring

---
+++ src/main/java/com/example/demo/BootProjApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProjApplication.class, args);
+		System.out.println("Bootify Spring ");
+	}
+
+}
